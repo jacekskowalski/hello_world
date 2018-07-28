c7201c653a9baa82dac4406c8f2cb7df0d2289ec
jacekskowalski <jsk_profil@hotmail.com>
2018-07-28 14:05:18 +0200

    Create Hello World app+Test controller

---
+++ src/main/java/com/project/hello_world/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.project.hello_world.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+   public String index() {
+       return "Hello World";
+    }
+}
